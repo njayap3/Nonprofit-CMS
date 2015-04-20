baaad830390073877c9e6670a89875a36e20bc55
Neha Jayaprakash <Neha_Jayaprakash@intuit.com>
2015-04-20 13:24:19 -0700

    Initial setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,16 +10,12 @@ module.exports = function (grunt) {
 	grunt.registerTask("default", "start a web server", function() {
 
 		var 
-			http = require("http"),
-			express = require("express"),
-			app = express(),
+			webServer = require("./web-server");
 			webServerConfig = grunt.config("webServer");
 
 		this.async();
 
-		app.use(express.static(webServerConfig.rootFolder));
-
-		http.createServer(app).listen(webServerConfig.port, function() {
+		webServer(webServerConfig, function() {
 			grunt
 				.log
 				.writeln("web server listening on port: " + webServerConfig.port);
